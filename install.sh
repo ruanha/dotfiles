8a5cbd432d209e8832e7245df70c345550cbd976
rune <rune_hartvig@hotmail.com>
2021-04-24 22:47:38 +0200

    add install fish

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # install script for my dotfiles
 echo begin installation...
+sudo apt-get update
 
 # make link to dotfiles/.vimrc
 if [ ~/.vimrc ]
@@ -9,3 +10,5 @@ then
 fi
 ln ./.vimrc ~/.vimrc
 
+# install fish - apt-get install is idempotent so it is safe to run, even if already installed
+sudo apt-get install fish
