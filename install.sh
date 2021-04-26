eeb79303707119f20fbcccf4dfa8a13dfffdc7c2
rune <rune_hartvig@hotmail.com>
2021-04-26 14:31:53 +0200

    mkdir conf.d before creating tmux.fish file

--- install.sh
+++ install.sh
@@ -13,11 +13,12 @@ ln ./.vimrc ~/.vimrc
 # install fish - apt-get install is idempotent so it is safe to run, even if already installed
 sudo apt-get install fish
 
-# add .fish dotfile
-ln ./tmux.fish ~/.config/fish/conf.d/tmux.fish
-
 # install tldr
 sudo apt-get install tldr
 
 # install tmux
 sudo apt-get install tmux
+
+# add .fish dotfile
+mkdir -p ~/.config/fish/conf.d
+ln ./tmux.fish ~/.config/fish/conf.d/tmux.fish
