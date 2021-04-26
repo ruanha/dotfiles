3f169ec53982b84f5ccc241c77c89bdb19475def
rune <rune_hartvig@hotmail.com>
2021-04-26 14:12:13 +0200

    add tmux

--- install.sh
+++ install.sh
@@ -13,5 +13,11 @@ ln ./.vimrc ~/.vimrc
 # install fish - apt-get install is idempotent so it is safe to run, even if already installed
 sudo apt-get install fish
 
+# add .fish dotfile
+ln ./tmux.fish ~/.config/fish/conf.d/tmux.fish
+
 # install tldr
 sudo apt-get install tldr
+
+# install tmux
+sudo apt-get install tmux
